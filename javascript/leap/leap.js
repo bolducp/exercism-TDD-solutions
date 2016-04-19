4279c62862d6a7e89529f67d77bf175b791fa02e
Paige Bolduc <paigebolduc@Roberts-MacBook-Air-2.local>
2016-04-19 19:44:53 -0400

    add leap challenge and initial solution

---
+++ javascript/leap/leap.js
@@ -0,0 +1,16 @@
+var Year = function(year) {
+  this.year = year;
+};
+
+Year.prototype.isLeap = function(input) {
+  if (this.year % 4 === 0 && this.year % 100 === 0 && this.year % 400 === 0) {
+    return true;
+  } else if (this.year % 4 === 0 && this.year % 100 === 0 ) {
+    return false;
+  } else if (this.year % 4 === 0) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = Year;
