c14c2c4f128259e60cdafb0adea7041de7455ce7
Paige Bolduc <paigebolduc@gmail.com>
2016-06-04 15:52:19 -0400

    add bob solution

---
+++ javascript/bob.js
@@ -0,0 +1,16 @@
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  input = input.trim();
+
+  if (input === input.toUpperCase() && input.match(/[a-z]/i)) {
+    return "Whoa, chill out!";
+  } else if (input[input.length - 1] === "?") {
+     return "Sure.";
+  } else if (input.length === 0) {
+    return "Fine. Be that way!";
+  }
+  return "Whatever.";
+}
+
+module.exports = Bob;
