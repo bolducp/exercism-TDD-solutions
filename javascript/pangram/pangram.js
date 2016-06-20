9484177d37802a136fbc914262f807787e7beaad
Paige Bolduc <paigebolduc@gmail.com>
2016-06-19 21:10:22 -0400

    add alternative solution to panagram challenge

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,17 +1,11 @@
 var Pangram = function(sentence){
   this.sentence = sentence;
-  this.sanitizedLetters = sentence.trim().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split("");
+  this.sanitizedLetters = sentence.trim().toLowerCase().replace(/[^a-zA-Z]/g,"");
+  this.letterSet = new Set(this.sanitizedLetters);
 };
 
 Pangram.prototype.isPangram = function(){
-  var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
-  for (var i in alphabet){
-    if (this.sanitizedLetters.indexOf(alphabet[i]) === -1){
-      return false;
-    }
-  }
-  return true;
+  return this.letterSet.size === 26;
 };
 
 module.exports = Pangram;
