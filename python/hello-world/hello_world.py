dfe87c9f8e8ea478195e626d63bd2c5ea9741cb0
Paige Bolduc <paigebolduc@gmail.com>
2016-07-18 22:17:18 -0400

    python solutions for hello-world and leap

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,10 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name='World'):
+    return 'Hello, %s!' % name
+
+
+
---
+++ python/leap/year.py
@@ -0,0 +1,8 @@
+def is_leap_year(year):
+		return is_divisible_by(year, 4) and \
+				(not is_divisible_by(year, 100) or is_divisible_by(year, 400))
+
+
+def is_divisible_by(num, denominator):
+		return num % denominator == 0
+	
