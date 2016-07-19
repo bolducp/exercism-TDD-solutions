2ffa76330fe47a764f7086cc0b0c0316e7662176
Paige Bolduc <paigebolduc@gmail.com>
2016-07-18 20:53:11 -0400

    refactor to use switch statement

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -3,25 +3,25 @@
 var BeerSong = function(){};
 
 BeerSong.prototype.verse = function(verseNum){
-  if (verseNum === 0) {
-      return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-  } else if (verseNum === 1) {
-      return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-  } else if (verseNum === 2) {
-      return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-  } else {
-      return `${verseNum} bottles of beer on the wall, ${verseNum} bottles of beer.\nTake one down and pass it around, ${verseNum - 1} bottles of beer on the wall.\n`;
+  switch (verseNum) {
+    case 0:
+        return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    case 1:
+        return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    case 2:
+        return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    default:
+        return `${verseNum} bottles of beer on the wall, ${verseNum} bottles of beer.\nTake one down and pass it around, ${verseNum - 1} bottles of beer on the wall.\n`;
   }
 };
 
-BeerSong.prototype.sing = function(start, end){
-    var end = typeof end !== 'undefined' ?  end: 0;
+BeerSong.prototype.sing = function(highBottle, lowBottle){
+    lowBottle = lowBottle ? lowBottle: 0;
     var song = [];
     
-    for (var i = start; i >= end; i --) {
+    for (var i = highBottle; i >= lowBottle; i--) {
         song.push(this.verse(i));
     }
-    
     return song.join("\n");
 };
 
