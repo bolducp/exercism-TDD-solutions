e7d5591d6afbbcbe1f8bd09252b1a98108fd813a
Paige Bolduc <paigebolduc@gmail.com>
2016-07-18 21:15:48 -0400

    add typescript solution for beer-song challenge

--- javascript/beer-song/beer-song.ts
+++ javascript/beer-song/beer-song.ts
@@ -1,19 +1,27 @@
 class BeerSong{
-  constructor(){}
+	constructor(){}
 
-  private finalVerse:string = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+	verse(verseNum:number):string {
+    	const specialCases = {
+			0: "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+			1: "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+			2: "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      	}
+      
+		if (verseNum in specialCases) {
+			return specialCases[verseNum]
+		}
+		return `${verseNum} bottles of beer on the wall, ${verseNum} bottles of beer.\nTake one down and pass it around, ${verseNum - 1} bottles of beer on the wall.\n`;
+	}
 
-  
-
-  verse(verseNum:number):string{
-
-  }
-
-  sing(start:number, end=0){
-
-  }
-
+	sing(highBottle:number, lowBottle = 0){
+		var song = [];
+    
+		for (var i = highBottle; i >= lowBottle; i--) {
+			song.push(this.verse(i));
+		}
+    	return song.join("\n");
+	}
 }
-
 
 module.exports = BeerSong;
