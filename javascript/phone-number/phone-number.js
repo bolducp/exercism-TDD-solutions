29a814c55fd26b85bb80b259092a638e6330ba81
Paige Bolduc <paigebolduc@gmail.com>
2016-07-30 23:20:41 -0400

    next version of js solution to phone-number challenge

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -3,51 +3,40 @@ var PhoneNumber = function(phoneNumber) {
 };
 
 PhoneNumber.prototype.number = function() {
-	if (isValidNumber(this.phoneNumber)) {
-		var sanitizedNumber = stripFormatters(this.phoneNumber);
-		if (sanitizedNumber.length === 10) {
-			return sanitizedNumber;
-		} else {
-			return sanitizedNumber.slice(1);
-		}
+	if (isValid10DigitNumber(this.phoneNumber)) {
+		return stripFormatters(this.phoneNumber);
+	} else if (isValid11DigitNumber(this.phoneNumber)) {
+		return stripFormatters(this.phoneNumber).slice(1);
 	}
 	return "0000000000";
 }
 
 PhoneNumber.prototype.areaCode = function() {
-	if (isValidNumber(this.phoneNumber)) {
-		if (stripFormatters(this.phoneNumber).length === 11) { 
-			return this.phoneNumber.slice(1, 4);
-		} else {
-			return this.phoneNumber.slice(0, 3);
-		}
+	if (isValid11DigitNumber(this.phoneNumber)) {
+		return this.phoneNumber.slice(1, 4);
+	} else if (isValid10DigitNumber(this.phoneNumber)) {
+		return this.phoneNumber.slice(0, 3);
 	}
 	return "000";
 }
 
 PhoneNumber.prototype.toString = function() {
-	if (isValidNumber(this.phoneNumber)) {
-		var formattedNumber = "";
-
-		if (stripFormatters(this.phoneNumber).length === 10) { 
-			formattedNumber += ("(" + this.areaCode() + ") " + this.number().slice(3, 6) + "-" + this.number().slice(6, 10));
-		} else {
-			formattedNumber += ("(" + this.areaCode() + ") " + this.number().slice(4, 7) + "-" + this.number().slice(7, 11));
-		}
-		return formattedNumber;
+	if (isValid10DigitNumber(this.phoneNumber)) {
+		return "(" + this.areaCode() + ") " + this.number().slice(3, 6) + "-" + this.number().slice(6, 10);
+	} else if (isValid11DigitNumber(this.phoneNumber)) {
+		return "(" + this.areaCode() + ") " + this.number().slice(4, 7) + "-" + this.number().slice(7, 11);
 	}
 	return "Invalid Number";
 }
 
-function isValidNumber(phoneNumber) { 
+function isValid10DigitNumber(phoneNumber) { 
 	var strippedNumber = stripFormatters(phoneNumber);
+	return strippedNumber.length === 10 && isNumeric(strippedNumber);
+}
 
-	if (strippedNumber.length === 10 || (strippedNumber.length === 11 && strippedNumber[0] == 1)) {
-		if (isNumeric(strippedNumber)) {
-			return true;
-		} 
-	}
-	return false;
+function isValid11DigitNumber(phoneNumber) { 
+	var strippedNumber = stripFormatters(phoneNumber);
+	return strippedNumber.length === 11 && strippedNumber[0] == 1 && isNumeric(strippedNumber);
 }
 
 function isNumeric(phoneNumber) {
