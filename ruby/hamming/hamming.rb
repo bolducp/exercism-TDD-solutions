ed1892652c6ae6ce2f6686a45a7401896a85ef16
Paige Bolduc <paigebolduc@gmail.com>
2016-08-20 10:49:15 -0500

    Add Ruby solution for Hammging challenge

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,14 @@
+class Hamming
+  class << self
+    def compute(strand1, strand2)
+	raise ArgumentError unless strand1.length == strand2.length
+  	counter = 0
+	strand1.chars.each_with_index do |value, index|
+	  if value != strand2.chars[index]
+	    counter += 1
+	  end 
+	end
+      counter
+    end
+  end
+end
