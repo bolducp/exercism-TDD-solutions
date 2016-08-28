ec1a9c615f383129251a4e6157d28fb1034b2422
Paige Bolduc <paigebolduc@gmail.com>
2016-08-28 11:34:54 -0500

    Fix indentation issues

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,13 @@
 class Hamming
   class << self
     def compute(strand1, strand2)
-			raise ArgumentError unless strand1.length == strand2.length
-				counter = 0
-			strand1.chars.each_with_index do |value, index|
-				if value != strand2.chars[index]
-					counter += 1
-				end 
-			end
+      raise ArgumentError unless strand1.length == strand2.length
+      counter = 0
+      strand1.chars.each_with_index do |value, index|
+        if value != strand2.chars[index]
+          counter += 1
+        end
+      end
       counter
     end
   end
