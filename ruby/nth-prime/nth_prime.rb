00d1c9b668446ddd587beed6359cf70431a334d5
Paige Bolduc <paigebolduc@gmail.com>
2016-10-04 23:09:50 -0400

    Refactor ruby solution of nth_prime.

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,20 +1,22 @@
 class Prime
 
   def self.nth(number)
-    raise ArgumentError.new if number < 1
+    raise ArgumentError.new("Number must be >= 1.") if number < 1
     primes = []
     counter = 1
+
     while primes.length < number do
       counter += 1
-      if prime?(counter)
-        primes << counter
-      end
+      primes << counter if prime?(counter)
     end
+
     primes.last
   end
 
+  private
   def self.prime?(num)
-    (2..num/2).none?{ |i| num % i == 0 }
+    upper_bound = Math.sqrt(num)
+    (2..upper_bound).none?{ |i| num % i == 0 }
   end
 
 end
