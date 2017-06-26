0182b9e21de539dde684c0c2dcd8e4851027f4a6
Paige Bolduc <paige@smartlogic.io>
2017-06-26 18:45:33 -0400

    Add initial Bob solution

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,30 @@
 defmodule Bob do
-  def hey(input) do
+def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+        question?(input) -> "Sure."
+        empty?(input) -> "Fine. Be that way!"
+        invalid_speech(input) -> "Whatever."
+        shouting?(input) -> "Whoa, chill out!"
+        true -> "Whatever."
     end
+  end
+
+  def invalid_speech(input) do
+    !(input =~ ~r/[[:alpha:]]/)
+  end
+
+  def empty?(input) do
+    input
+    |> String.strip == ""
+  end
+
+  def shouting?(input) do
+    input
+    |> String.upcase == input
+  end
+
+  def question?(input) do
+    input
+    |> String.last == "?"
   end
 end
