d5c1f11fcf9253c2f8b00f06d61829bda068ecc1
Paige Bolduc <paigebolduc@gmail.com>
2017-06-27 20:58:58 -0400

    Add initial secret handshake solution

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -15,6 +15,31 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    digits =
+      Integer.digits(code, 2) |> Enum.reverse
+
+    wink([], Enum.at(digits, 0))
+    |> double_blink(Enum.at(digits, 1))
+    |> close_your_eyes(Enum.at(digits, 2))
+    |> jump(Enum.at(digits, 3))
+    |> reverse(Enum.at(digits, 4))
+
   end
+
+  defp reverse(commands, 1), do: Enum.reverse(commands)
+  defp reverse(commands, _num), do: commands
+
+  defp jump(commands, 1), do: commands ++ ["jump"]
+  defp jump(commands, _num), do: commands
+
+  defp close_your_eyes(commands, 1), do: commands ++ ["close your eyes"]
+  defp close_your_eyes(commands, _num), do: commands
+
+  defp double_blink(commands, 1), do: commands ++ ["double blink"]
+  defp double_blink(commands, _num), do: commands
+
+  defp wink(commands, 1), do: commands ++ ["wink"]
+  defp wink(commands, _num), do: commands
 end
 
+SecretHandshake.commands(1)
