d2e526b3484de639e2dcc37f90c041533e88bf04
Paige Finkelstein <paigelfink@gmail.com>
2019-05-07 09:33:04 -0400

    Revise to handle all parsing in init method

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -3,11 +3,11 @@ class Matrix(object):
         self.matrix = self.parse_string(matrix_string)
 
     def row(self, index):
-        return [int(num) for num in self.matrix[index - 1]]
+        return [num for num in self.matrix[index - 1]]
 
     def column(self, index):
-        return [int(row[index -1]) for row in self.matrix]
+        return [row[index -1] for row in self.matrix]
 
     def parse_string(self, matrix_string):
         rows = matrix_string.split("\n")
-        return [row.split() for row in rows]
+        return [[int(num) for num in row.split()] for row in rows]
