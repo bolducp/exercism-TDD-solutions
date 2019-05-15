189183112e5ed9e7e0fba17b0472359464e008a2
Paige Finkelstein <paigelfink@gmail.com>
2019-05-15 17:13:11 -0500

    refactor straight scoring

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,7 +1,6 @@
 from functools import partial
 
 # Score categories
-# Change the values as you see fit
 YACHT = 'yacht'
 ONES = 'ones'
 TWOS = 'twos'
@@ -26,8 +25,8 @@ def score(dice, category):
         SIXES: partial(score_num, num=6),
         FULL_HOUSE: score_full_house,
         FOUR_OF_A_KIND: score_four_of_a_kind,
-        LITTLE_STRAIGHT: score_little_straight,
-        BIG_STRAIGHT: score_big_straight,
+        LITTLE_STRAIGHT: partial(score_straight, straight=[1, 2, 3, 4, 5]),
+        BIG_STRAIGHT: partial(score_straight, straight=[2, 3, 4, 5, 6]),
         CHOICE: score_choice,
     }
 
@@ -59,13 +58,8 @@ def score_four_of_a_kind(dice):
             return die * 4
     return 0
 
-def score_little_straight(dice):
-    if sorted(dice) == [1, 2, 3, 4, 5]:
-        return 30
-    return 0
-
-def score_big_straight(dice):
-    if sorted(dice) == [2, 3, 4, 5, 6]:
+def score_straight(straight, dice):
+    if sorted(dice) == straight:
         return 30
     return 0
 
