65561cbff3a599359cbd385553497eee4d709b3a
Paige Finkelstein <paigelfink@gmail.com>
2019-05-16 12:41:47 -0400

    Refactor out additional empty check

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -7,10 +7,8 @@ def saddle_points(matrix):
 
     if len({len(row) for row in matrix}) > 1:
         raise ValueError("Invalid Matrix")
-    if not matrix.size:
-        return set()
 
-    for row, col in product(range(matrix.shape[0]), range(matrix.shape[1])):
+    for row, col in product(*[range(i) for i in matrix.shape]):
         if max(matrix[row]) == min(matrix[:, col]):
             saddles.append((row + 1, col + 1))
 
