98cdfcf70a38a02328e2c6bde4f168196b42f71a
Paige Finkelstein <paigelfink@gmail.com>
2019-05-16 12:37:09 -0400

    Refactor to use numpy

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,14 +1,17 @@
+import numpy as np
+from itertools import product
+
 def saddle_points(matrix):
     saddles = []
+    matrix = np.array(matrix)
 
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise ValueError("Invalid Matrix")
+    if len({len(row) for row in matrix}) > 1:
+        raise ValueError("Invalid Matrix")
+    if not matrix.size:
+        return set()
 
-    for row_i, row in enumerate(matrix):
-        for index, point in enumerate(row):
-            if max(row) == point:
-                if min(col for row in matrix for i, col in enumerate(row) if i == index) == point:
-                    saddles.append((row_i + 1, index + 1))
+    for row, col in product(range(matrix.shape[0]), range(matrix.shape[1])):
+        if max(matrix[row]) == min(matrix[:, col]):
+            saddles.append((row + 1, col + 1))
 
     return set(saddles)
