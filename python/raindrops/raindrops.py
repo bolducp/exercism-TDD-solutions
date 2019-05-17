c412bfd2e27d5ec2a3331c8dcb82ef35cac96abb
Paige Finkelstein <paigelfink@gmail.com>
2019-05-16 21:11:30 -0400

    stylistic fixes to raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,8 +1,8 @@
 sounds = {
     3: "Pling",
     5: "Plang",
-    7: "Plong"
+    7: "Plong",
 }
 
 def raindrops(number):
-    return  "".join([sounds[num] for num in sounds if number % num == 0]) or str(number)
+    return "".join([sounds[num] for num in sounds if number % num == 0]) or str(number)
