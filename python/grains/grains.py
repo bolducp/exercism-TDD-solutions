90f15e468e9df800288356429320ae81e9d632eb
Paige Finkelstein <paigelfink@gmail.com>
2019-05-17 15:57:36 -0400

    refactor

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,10 +1,11 @@
 def on_square(integer_number):
-    if integer_number < 1 or integer_number > 64:
-        raise ValueError("integer_number must be between 1 and 64")
+    check_valid_input(integer_number)
     return 2**(integer_number - 1)
 
-
 def total_after(integer_number):
+    check_valid_input(integer_number)
+    return 2**integer_number - 1
+
+def check_valid_input(integer_number):
     if integer_number < 1 or integer_number > 64:
         raise ValueError("integer_number must be between 1 and 64")
-    return sum([on_square(num) for num in range(1, integer_number + 1)])
