2d005bfff4c24c9a8749d55e43ee4e014f47f0ba
Paige Finkelstein <paigelfink@gmail.com>
2019-05-21 13:21:24 -0400

    Initial triangle solution

---
+++ python/triangle/triangle.py
@@ -0,0 +1,17 @@
+def is_equilateral(sides):
+    return check_is_triange(sides) and len(set(sides)) == 1
+
+def is_isosceles(sides):
+    return check_is_triange(sides) and len(set(sides)) < 3
+
+def is_scalene(sides):
+    return check_is_triange(sides) and len(set(sides)) == 3
+
+def check_is_triange(sides):
+    sorted_sides = sorted(sides)
+
+    if 0 in sides:
+        return False
+    elif sorted_sides[2] > (sorted_sides[0] + sorted_sides[1]):
+        return False
+    return True
