200507f20002b9737216e6282a313031a8a85ada
Paige Finkelstein <paigelfink@gmail.com>
2019-06-04 16:53:47 -0400

    initial solution

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,14 @@
+def collatz_steps(number):
+    if number < 1:
+        raise ValueError("Input must be a positive number")
+
+    counter = 0
+
+    while number > 1:
+        counter += 1
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = (number * 3) + 1
+
+    return counter
